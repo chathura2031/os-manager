c884387e4ed7574bc09d79b8eb8a8a15cd685c73
Chathura <chathura2031+github@outlook.com>
2024-10-12 20:12:53 +1100

    Serialisation test

--- OSManager.Daemon/Program.cs
+++ OSManager.Daemon/Program.cs
@@ -1,15 +1,41 @@
 ﻿using System.IO.Pipes;
+using OSManager.Daemon;
+using ProtoBuf;
 
 await StartServer(new CancellationToken());
 Task.Delay(1000).Wait();
 
 async Task StartServer(CancellationToken stoppingToken)
 {
+    var person = new Person {
+        Id = 12345, Name = "Fred",
+        Address = new Address {
+            Line1 = "Flat 1",
+            Line2 = "The Meadows"
+        }
+    };
+
+    byte[] bytes;
+    string s;
+    {
+        MemoryStream stream = new();
+        Serializer.Serialize(stream, person);
+        bytes = stream.ToArray();
+        s = System.Text.Encoding.UTF8.GetString(bytes);
+    }
+
+    // TODO: Figure out how to serialise and deserialize
+    Person person1;
+    {
+        MemoryStream stream = new(bytes);
+        person1 = Serializer.Deserialize<Person>(stream);
+    }
+    
     NamedPipeServerStream server = new("PipesOfPiece");
     await server.WaitForConnectionAsync();
     StreamReader reader = new(server);
     StreamWriter writer = new(server);
-    
+
     while (!stoppingToken.IsCancellationRequested)
     {
         string? line = reader.ReadLine();
