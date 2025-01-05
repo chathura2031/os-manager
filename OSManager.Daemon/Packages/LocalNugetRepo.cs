eb79171f16e74791a0dde15ea175806008e17256
Chathura <chathura2031+github@outlook.com>
2025-01-05 18:38:00 +1100

    Update local nuget repo package

--- OSManager.Daemon/Packages/LocalNugetRepo.cs
+++ OSManager.Daemon/Packages/LocalNugetRepo.cs
@@ -1,128 +1,151 @@
 using System.Xml;
 using OSManager.Core.Enums;
-using OSManager.Core.Extensions;
-using OSManager.Daemon.Extensions;
 
 namespace OSManager.Daemon.Packages;
 
-public class LocalNugetRepo : IPackage
+public class LocalNugetRepo : BasePackage
 {
+    #region public static members
     public static readonly LocalNugetRepo Instance = new();
+    #endregion
 
-    public Package Package { get; } = Package.LocalNugetRepo;
-
-    public List<IPackage> Dependencies { get; } = [];
-
-    public List<IPackage> OptionalExtras { get; } = [];
+    #region public members
+    public override Package Package { get; } = Package.LocalNugetRepo;
+    public override List<IPackage> Dependencies { get; } = [];
+    public override List<IPackage> OptionalExtras { get; } = [];
+    #endregion
     
-    public int Install(int stage, string data)
+    #region protected members
+    protected override List<Func<string, InstallStepReturnData>> InstallSteps { get; }
+    protected override List<Func<int>> ConfigureSteps { get; }
+    protected override List<Func<int>> BackupConfigurationSteps { get; }
+    #endregion
+    
+    #region private members
+    private string NugetDirPath => Path.Join(Utilities.HomeDirectory, ".nuget");
+    private string LocalNugetRepoPath => Path.Join(NugetDirPath, "local-packages");
+    private string NugetConfigDirPath => Path.Join(NugetDirPath, "NuGet");
+    private string NugetConfigFilePath => Path.Join(NugetConfigDirPath, "NuGet.Config");
+    private string NugetTemplateConfigFilePath => Path.Join(BackupDirPath, "NuGet.Config");
+    #endregion
+
+    #region ctor
+    private LocalNugetRepo()
     {
-        int statusCode = 0;
-        switch (stage)
+        InstallSteps = [];
+        ConfigureSteps = [CreateLocalNugetRepo];
+        BackupConfigurationSteps = [BackupNugetConfig];
+    }
+    #endregion
+    
+    #region private methods
+    private void UpdateNugetConfigFile()
+    {
+        if (!File.Exists(NugetTemplateConfigFilePath))
         {
-            case 1:
-            {
-                Utilities.BashStack.PushInstallStage(stage + 1, Package.Name());
-                this.InstallDependencies();
-                break;
-            }
-            case 2:
-            {
-                break;
-            }
-            default:
-                throw new ArgumentException($"{Package.PrettyName()} does not have {stage} installation.");
+            return;
         }
         
-        return statusCode;
-    }
+        Directory.CreateDirectory(LocalNugetRepoPath);
+        Directory.CreateDirectory(NugetConfigDirPath);
 
-    public int Configure(int stage)
-    {
-        int statusCode = 0;
-        switch (stage)
+        // Create the nuget config file from a template if it doesn't exist
+        if (!File.Exists(NugetConfigFilePath))
         {
-            case 1:
-            {
-                string nugetPath = Path.Join(Utilities.HomeDirectory, ".nuget");
-                string localNugetPath = Path.Join(nugetPath, "local-packages");
-                
-                Directory.CreateDirectory(localNugetPath);
-
-                // Create the nuget config file from a template if it doesn't exist
-                string nugetConfigPath = Path.Join(nugetPath, "NuGet/NuGet.Config");
-                if (!File.Exists(nugetConfigPath))
-                {
-                    File.Copy(Path.Join(Utilities.BackupDirectory, Package.Name(), "NuGet.Config"),
-                        nugetConfigPath);
-                }
-
-                // Add the local nuget repository directory as a source
-                XmlDocument doc = new();
-                doc.Load(nugetConfigPath);
-
-                XmlNode? sources = doc.DocumentElement!.SelectSingleNode("/configuration/packageSources");
-                if (sources == null)
-                {
-                    throw new Exception("Invalid NuGet.Config file. Could not find sources.");
-                }
-
-                XmlNodeList stuff = ((XmlNode)sources).SelectNodes("add")!;
-                bool entryAdded = false;
-                foreach (XmlNode thing in stuff)
-                {
-                    if (thing.Attributes!["key"]!.Value != "LocalRepo")
-                    {
-                        continue;
-                    }
-                    
-                    if (thing.Attributes["value"]!.Value != localNugetPath)
-                    {
-                        thing.Attributes["value"]!.Value = localNugetPath;
-                    }
-
-                    entryAdded = true;
-                }
-
-                if (!entryAdded)
-                {
-                    XmlElement node = doc.CreateElement("add");
-                    node.SetAttribute("key", "LocalRepo");
-                    node.SetAttribute("value", localNugetPath);
-                    
-                    sources.AppendChild(node);
-                    entryAdded = true;
-                }
-                
-                // Update the nuget config file
-                doc.Save(nugetConfigPath);
-                
-                // Download the nuget packages
-                Utilities.DownloadFromUrl(
-                    "https://raw.githubusercontent.com/chathura2031/persistent-tools/master/PersistentTools.Stack/bin/Release/latest.nupkg",
-                    $"PersistentTools.Stack.latest.nupkg",
-                    out string filePath,
-                    localNugetPath
-                );
-                
-                break;
-            }
-            default:
-                throw new ArgumentException($"{Package.PrettyName()} does not have {stage} stages of configuration.");
+            File.Copy(NugetTemplateConfigFilePath, NugetConfigFilePath);
         }
 
-        return statusCode;
-    }
+        // Add the local nuget repository directory as a source
+        XmlDocument doc = new();
+        doc.Load(NugetConfigFilePath);
 
-    public int BackupConfig(int stage)
-    {
-        int statusCode = 0;
-        switch (stage)
+        XmlNode? sources = doc.DocumentElement!.SelectSingleNode("/configuration/packageSources");
+        if (sources == null)
         {
-            default:
-                throw new ArgumentException($"{Package.PrettyName()} does not have {stage} stages of configuration backup.");
+            throw new Exception("Invalid NuGet.Config file. Could not find sources.");
+        }
+
+        XmlNodeList sourceNodes = ((XmlNode)sources).SelectNodes("add")!;
+        bool entryAdded = false;
+        foreach (XmlNode sourceNode in sourceNodes)
+        {
+            if (sourceNode.Attributes!["key"]!.Value != "LocalRepo")
+            {
+                continue;
+            }
+            
+            if (sourceNode.Attributes["value"]!.Value != LocalNugetRepoPath)
+            {
+                sourceNode.Attributes["value"]!.Value = LocalNugetRepoPath;
+            }
+
+            entryAdded = true;
+        }
+
+        if (!entryAdded)
+        {
+            XmlElement node = doc.CreateElement("add");
+            node.SetAttribute("key", "LocalRepo");
+            node.SetAttribute("value", LocalNugetRepoPath);
+            
+            sources.AppendChild(node);
+            entryAdded = true;
         }
         
-        return statusCode;
+        // Update the nuget config file
+        doc.Save(NugetConfigFilePath);
     }
+
+    private void DownloadNugetPackages()
+    {
+        // Download the nuget packages
+        Utilities.DownloadFromUrl(
+            "https://raw.githubusercontent.com/chathura2031/persistent-tools/master/PersistentTools.Stack/bin/Release/latest.nupkg",
+            $"PersistentTools.Stack.latest.nupkg",
+            out _,
+            LocalNugetRepoPath
+        );
+    }
+    
+    private int CreateLocalNugetRepo()
+    {
+        UpdateNugetConfigFile();
+        DownloadNugetPackages();
+
+        return 0;
+    }
+
+    private int BackupNugetConfig()
+    {
+        if (!File.Exists(NugetConfigFilePath))
+        {
+            return 0;
+        }
+        
+        Directory.CreateDirectory(BackupDirPath);
+        
+        XmlDocument doc = new();
+        doc.Load(NugetConfigFilePath);
+
+        XmlNode? sources = doc.DocumentElement!.SelectSingleNode("/configuration/packageSources");
+        if (sources == null)
+        {
+            throw new Exception("Invalid NuGet.Config file. Could not find sources.");
+        }
+
+        XmlNodeList sourceNodes = ((XmlNode)sources).SelectNodes("add")!;
+        foreach (XmlNode sourceNode in sourceNodes)
+        {
+            if (sourceNode.Attributes!["key"]!.Value == "LocalRepo")
+            {
+                sources.RemoveChild(sourceNode);
+                break;
+            }
+        }
+
+        doc.Save(NugetTemplateConfigFilePath);
+        
+        return 0;
+    }
+    #endregion
 }
