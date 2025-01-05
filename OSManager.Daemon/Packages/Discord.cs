0ac6ac8bfcf35074b81ac822f23e198f58281530
Chathura <chathura2031+github@outlook.com>
2025-01-05 17:46:43 +1100

    Simplify discord package

--- OSManager.Daemon/Packages/Discord.cs
+++ OSManager.Daemon/Packages/Discord.cs
@@ -1,90 +1,68 @@
 using OSManager.Core.Enums;
 using OSManager.Core.Extensions;
-using OSManager.Daemon.Extensions;
 
 namespace OSManager.Daemon.Packages;
 
-public class Discord : IPackage
+public class Discord : BasePackage
 {
+    #region public static members
     public static readonly Discord Instance = new();
+    #endregion
 
-    public Package Package { get; } = Package.Discord;
+    #region public members
+    public override Package Package { get; } = Package.Discord;
+    public override List<IPackage> Dependencies { get; } = [];
+    public override List<IPackage> OptionalExtras { get; } = [];
+    #endregion
     
-    public List<IPackage> Dependencies { get; } = [];
+    #region protected members
+    protected override List<Func<string, InstallStepReturnData>> InstallSteps { get; }
+    protected override List<Func<int>> ConfigureSteps { get; }
+    protected override List<Func<int>> BackupConfigurationSteps { get; }
+    #endregion
     
-    public List<IPackage> OptionalExtras { get; } = [];
-
-    private int DownloadPackage(int verbosity, out string filePath)
+    #region ctor
+    private Discord()
     {
-        // Download the debian package
-        if (verbosity > 0)
-        {
-            Console.WriteLine("Downloading debian package...");
-        }
+        InstallSteps = [DownloadAndInstallDebFile, DeleteDebFile];
+        ConfigureSteps = [];
+        BackupConfigurationSteps = [];
+    }
+    #endregion
 
+    #region private methods
+    private int DownloadDebFile(out string filePath)
+    {
         int statusCode = Utilities.DownloadFromUrl("https://discord.com/api/download?platform=linux&format=deb",
             $"{Package.Name()}.deb", out filePath, $"/tmp/osman-{Guid.NewGuid()}");
-
+        
+        if (statusCode != 0)
+        {
+            DeleteDebFile(filePath);
+        }
+        
         return statusCode;
     }
-    
-    private int DeletePackage(int verbosity, string filePath)
+
+    private InstallStepReturnData DownloadAndInstallDebFile(string data)
     {
-        // Delete the debian package
-        if (verbosity > 0)
+        int statusCode = DownloadDebFile(out string filePath);
+        if (statusCode != 0)
         {
-            Console.WriteLine("Deleting debian package...");
+            return new InstallStepReturnData(statusCode, [], null);
         }
+        
+        return new InstallStepReturnData(
+            0,
+            [() => Utilities.BashStack.PushBashCommand($"apt install -y --fix-broken {filePath}", true)],
+            filePath
+        );
+    }
+
+    private InstallStepReturnData DeleteDebFile(string filePath)
+    {
         File.Delete(filePath);
-
-        return 0;
+        return new InstallStepReturnData(0, [], null);
     }
-
-    public int Install(int stage, string data)
-    {
-        int statusCode = 0;
-        switch (stage)
-        {
-            case 1:
-                Utilities.BashStack.PushInstallStage(stage + 1, Package.Name());
-                this.InstallDependencies();
-                break;
-            case 2:
-                statusCode = DownloadPackage(2, out string filePath);
-                if (statusCode != 0)
-                {
-                    Console.WriteLine("Failed to download debian file");
-                    File.Delete(filePath);
-                    return 1;
-                }
-                
-                Utilities.RunInReverse([
-                    () => Utilities.BashStack.PushBashCommand($"apt install -y --fix-broken {filePath}", true),
-                    () => Utilities.BashStack.PushInstallStage(stage + 1, Package.Name(), filePath),
-                ]);
-                break;
-            case 3:
-                statusCode = DeletePackage(2, data);
-                if (statusCode != 0)
-                {
-                    Console.WriteLine("Failed to delete debian file");
-                    return 1;
-                }
-                break;
-            default:
-                throw new ArgumentException($"{Package.PrettyName()} does not have {stage} stages of installation.");
-        }
-
-        return statusCode;
-    }
-
-    public int Configure(int stage)
-    {
-        return 0;
-    }
-
-    public int BackupConfig(int stage)
-    {
-        return 0;
-    }
+    #endregion
 }
